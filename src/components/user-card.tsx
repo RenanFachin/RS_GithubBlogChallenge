da75c9bfe27106a7a02fe76976d8713f5dbedafc
Renan <renan.fachin@hotmail.com>
2023-12-01 00:50:36 -0300

    feat: Cria requisição para github

--- src/components/user-card.tsx
+++ src/components/user-card.tsx
@@ -1,22 +1,25 @@
 import { ArrowSquareOut, GithubLogo, Buildings, Users } from 'phosphor-react'
+import { GithubDataProps } from '../pages/Blog'
 
+interface UserCardProps {
+  userInfo: GithubDataProps
+}
 
-export function UserCard() {
+export function UserCard({ userInfo }: UserCardProps) {
   return (
-    <div className="max-w-4xl w-full bg-base-profile py-8 pl-10 pr-8 absolute bottom-0 
-      translate-y-1/2 rounded-xl shadow-lg">
+    <div className="max-w-4xl w-full bg-base-profile py-8 pl-10 pr-8 -mt-24 rounded-xl shadow-lg z-20 mx-auto">
 
       <div className="flex gap-8">
-        <img src="https://github.com/RenanFachin.png" alt="" className="w-36 h-36" />
+        <img src={userInfo.avatar_url} alt="" className="w-36 h-36" />
 
         <div className="mt-2 flex flex-col items-start">
           <div className="flex items-center justify-between w-full">
             <h1 className="flex-1 text-base-title text-2xl leading-tight font-bold">
-              Renan Fachin
+              {userInfo.username}
             </h1>
 
             <a
-              href={'http://github.com/RenanFachin'}
+              href={userInfo.html_url}
               className='flex gap-2 text-blue cursor-pointer hover:underline'
               target='_blank'
             >
@@ -26,23 +29,23 @@ export function UserCard() {
           </div>
 
           <span className='text-base-text leading-relaxed mt-2'>
-            Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.
+            {userInfo.bio}
           </span>
 
           <div className='mt-6 flex items-center justify-center gap-6'>
             <div className='flex items-center gap-2'>
               <GithubLogo className='w-5 h-5 text-base-label' />
-              <p className='text-base-subtitle leading-relaxed'>RenanFachin</p>
+              <p className='text-base-subtitle leading-relaxed'>{userInfo.login}</p>
             </div>
 
             <div className='flex items-center gap-2'>
               <Buildings className='w-5 h-5 text-base-label' />
-              <p className='text-base-subtitle leading-relaxed'>PUC-RS</p>
+              <p className='text-base-subtitle leading-relaxed'>{userInfo.location}</p>
             </div>
 
             <div className='flex items-center gap-2'>
               <Users className='w-5 h-5 text-base-label' />
-              <p className='text-base-subtitle leading-relaxed'>52 seguidores</p>
+              <p className='text-base-subtitle leading-relaxed'>{userInfo.followers} seguidores</p>
             </div>
           </div>
         </div>
--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -1,10 +1,50 @@
+import { useEffect, useState } from "react";
 import { Header } from "../components/Header";
+import { UserCard } from "../components/user-card";
+import { api } from "../lib/axios";
+
+
+// api.github.com/users/${username}
+export interface GithubDataProps {
+  avatar_url: string
+  bio: string
+  username: string
+  location: string
+  followers: number
+  html_url: string
+  login: string
+}
 
 export function Blog() {
+  const [userGithubData, setUserGithubData] = useState<GithubDataProps>({} as GithubDataProps)
+
+  async function fetchGithubData() {
+    const response = await api.get('/users/RenanFachin')
+
+    const data = response.data
+
+    setUserGithubData({
+      username: data.username,
+      avatar_url: data.avatar_url,
+      bio: data.bio,
+      followers: data.followers,
+      html_url: data.html_url,
+      location: data.location,
+      login: data.login
+    })
+  }
+
+
+  useEffect(() => {
+    fetchGithubData()
+  }, [])
+
   return (
     <div className="h-screen">
       <Header />
 
+      <UserCard userInfo={userGithubData} />
     </div>
   )
 }
+
