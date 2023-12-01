10ab44761b283500a856cb4d5fe3b65619807d81
Renan <renan.fachin@hotmail.com>
2023-11-30 23:18:33 -0300

    style: Instalação e configuração de fonte personalizada

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,17 @@
 /** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme'
+
+
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'sans': ['Nunito', defaultTheme.fontFamily.sans]
+    },
     extend: {},
   },
   plugins: [],
